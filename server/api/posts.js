ef5d04e831b9a9c8b8b234164b4af08786aeba86
Kaushik Nagaraj <kaushikstan@gmail.com>
2017-12-23 17:03:49 -0500

    GET APIs for Posts

--- server/api/posts.js
+++ server/api/posts.js
@@ -2,14 +2,42 @@ const router = require('express').Router()
 const {Post} = require('../db/models')
 module.exports = router
 
-GET all post
-GET by user_id
-GET by tag
-
+// GET all post
+// http://localhost:8080/api/posts
 router.get('/', (req, res, next) => {
-  Post.findAll({
+  Post.findAll({})
+  .then(posts => res.json(posts))
+  .catch(next)
+})
 
+// GET by user_id
+// http://localhost:8080/api/posts/:user_id
+router.get('/:user_id', (req, res, next) => {
+  Post.findAll({
+    where: {
+      user_id: req.params.user_id
+    }
   })
   .then(posts => res.json(posts))
   .catch(next)
 })
+
+// GET by 1 tag
+// http://localhost:8080/api/posts/:tag
+router.get('/:tag', (req, res, next) => {
+  Post.findAll({
+    where: {
+      tag: {
+        [Op.in]: req.params.tag
+      }
+    }
+  })
+  .then(posts => res.json(posts))
+  .catch(next)
+})
+
+// POST
+
+// PUT
+
+// DELETE
