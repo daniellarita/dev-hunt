3f470ed0e97fe17178ac998cace0e335ab8e4be9
Danni Baxter <dbaxter@wework.com>
2018-01-04 20:13:28 -0500

    post from front end

--- client/components/Post/CreatePost.js
+++ client/components/Post/CreatePost.js
@@ -3,35 +3,57 @@ import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 
 class CreatePost extends React.Component {
+  state = {
+    title: '',
+    url: '',
+    note: ''
+  }
 
-  render () {
+  handleTitleChange = (e) => {
+    this.setState({title: e.target.value})
+  }
+
+  handleUrlChange = (e) => {
+    this.setState({url: e.target.value})
+  }
+
+  handleNoteChange = (e) => {
+    this.setState({note: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render (props) {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
-          Title:
-          <input type="text" name="title" />
+        Title:
+        <input onChange={this.handleTitleChange} type="text" name="title" />
         </label>
         <label>
-          URL:
-          <input type="text" name="url" />
+        URL:
+        <input onChange={this.handleUrlChange} type="text" name="url" />
         </label>
         <label>
-          Note:
-          <textarea type="text" name="note" />
+        Note:
+        <textarea onChange={this.handleNoteChange} type="text" name="note" />
         </label>
         <button type="submit" value="Submit">
-          Submit
+        Submit
         </button>
       </form>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    post: state.post
-  }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//     post: state.post
+//   }
+// }
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
