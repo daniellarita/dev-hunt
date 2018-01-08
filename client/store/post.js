85fecfb900cce588da9555432047ef9bc625e8a7
Danni Baxter <dbaxter@wework.com>
2018-01-08 15:38:09 -0500

    remove unused redux code

--- client/store/post.js
+++ client/store/post.js
@@ -1,36 +1,11 @@
 import axios from 'axios'
 
-export const GET_POST = 'GET_POST'
-export const RECEIVE_POST = 'RECEIVE_POST'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-
-// action creators
-export function setPosts(posts) {
-  return {
-    type: GET_POST,
-    posts
-  }
-}
-
-export function receivePost(post) {
-  return {
-    type: RECEIVE_POST,
-    post
-  }
-}
 
 export const receivePosts = posts => ({
   type: RECEIVE_POSTS,
   posts
 })
-
-export function fetchPosts() {
-  return function(dispatch, getState) {
-    axios.get(`/api/posts`)
-    .then(results => dispatch(setPosts(results.data)))
-    .catch(console.log)
-  }
-}
 
 export const addNewPost = post => {
   return (dispatch, getState) => {
@@ -50,20 +25,13 @@ export const addNewPost = post => {
 };
 
 const initialState = {
-  posts: [],
-  selected: {}
+  posts: []
 }
 
 // reducer
 export default function reducer(state = initialState, action) {
   const newState = Object.assign({}, state)
   switch(action.type) {
-    case GET_POST:
-      newState.posts = action.posts
-      break
-    case RECEIVE_POST:
-      newState.selected = action.post
-      break
     case RECEIVE_POSTS:
       newState.posts = action.posts
       break
